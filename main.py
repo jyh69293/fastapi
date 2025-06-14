6ddc3fda3b2d7fe8e41eadde1b15135c7d806aa3
jyh69293 <50835244+jyh69293@users.noreply.github.com>
2025-06-14 16:24:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
