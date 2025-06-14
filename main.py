6d65829dd1b57597ca915bd29ef7f9f423edec4f
jyh69293 <50835244+jyh69293@users.noreply.github.com>
2025-06-14 16:15:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index2.html", {"request": request})
