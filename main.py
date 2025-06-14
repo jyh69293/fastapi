7fe3946685a2f56d7576008d8129b8ea776779a8
jyh69293 <50835244+jyh69293@users.noreply.github.com>
2025-06-14 16:13:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
