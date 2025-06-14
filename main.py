2f311f870d04933e0b92111d797cce5b08dc3733
jyh69293 <50835244+jyh69293@users.noreply.github.com>
2025-06-14 21:25:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from models import Base, Schedule
 from database import engine, SessionLocal
 from fastapi.middleware.cors import CORSMiddleware
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+app = FastAPI()
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+Base.metadata.create_all(bind=engine)
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # 개발용 전체 허용
@@ -16,11 +23,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-app = FastAPI()
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
-Base.metadata.create_all(bind=engine)
-
 def get_db():
     db = SessionLocal()
     try:
